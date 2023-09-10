6247f015e5f8e4cdd8066c62040a4ea916654e93
Clover <z.omran2007@gmail.com>
2023-09-10 13:32:41 +0300

    Fixed loading bug

--- script.js
+++ script.js
@@ -471,10 +471,28 @@ window.location.href = "piano/piano.html";
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-closeSlotsFunction()
+document.addEventListener("DOMContentLoaded", () => {
+    const backgroundInput = document.getElementById("backgroundInput");
 
+    backgroundInput.addEventListener("change", handleBackgroundUpload);
 });
+
+function handleBackgroundUpload(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const uploadedBackground = e.target.result;
+            applyUploadedBackground(uploadedBackground);
+            saveBackgroundToLocalStorage(uploadedBackground);
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
+
 
 let currentAudio = null;
 
